3100269343fffa309825be2cbb22d60eff4d7e75
Kushagra <kushagra3288@gmail.com>
2023-04-23 18:35:02 +0530

    added downloading functionality

--- src/Components/Home/Body.jsx
+++ src/Components/Home/Body.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { saveAs } from "file-saver";
 
 const Body = ({ gifs }) => {
+  const downloadGif = (title, url) => {
+    console.log("clicked");
+    saveAs(url, `${title}.gif`);
+  };
+
   return (
     <div className="bg-white mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <h2 className="text-2xl text-center font-bold tracking-tight text-gray-900">Gif's for you</h2>
@@ -10,7 +16,7 @@ const Body = ({ gifs }) => {
       ) : (
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {gifs.map((gif) => (
-            <div key={gif.id} className="group relative">
+            <div key={gif.id} className=" relative">
               <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
                   src={gif.url}
@@ -26,7 +32,27 @@ const Body = ({ gifs }) => {
                     </a>
                   </h3>
                 </div>
-                <p className="text-sm font-medium text-gray-900">something</p>
+                <button
+                  onClick={() => {
+                    downloadGif(gif.title, gif.url);
+                  }}
+                  className="z-10 p-2 bg-gray-200 text-sm font-medium text-gray-900"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                    />
+                  </svg>
+                </button>
               </div>
             </div>
           ))}
